dfeddb9558735e99d6bac8dcb4fae33a8ade6fbb
Chris Vickery <c4@swirl.com>
2015-04-15 20:28:27 -0400

    Update, testing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 var pmx = require('pmx');
+var config = require('../config');
 
 pmx.action('upgrade_webapp', function(reply) {
-
+	var ls = require('child_process').spawn('git', ['pull'], {
+		cwd: config.client.cwd
+	});
+	ls.on('close', function (code) {
+		reply(code);
+	});
 });
 
 pmx.action('npm install server', function(reply) {
